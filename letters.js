7d3c39a640a8b613938fcb04f6eceba719550e79
Hannah Phillips <135017357+hannahbradee@users.noreply.github.com>
2023-08-17 11:39:53 +1200

    Update letters.js
    i have added in the parameters for the letter A

--- letters.js
+++ letters.js
@@ -4,21 +4,31 @@ const alphabet = {
     "offsetx": 0,
     "offsety": -40,
 
-     "triSize":1.75,
+     "triSize":2,
      "triOffsetx":0,
      "triOffsety":60,
      "triRotation":0 
   },
-//   "A": {
-//     "size": 40,
-//     "offsetx": 0,
-//     "offsety": 17
-//   },
-//   "B": {
-//     "size": 75,
-//     "offsetx": 0,
-//     "offsety": -70
-//   },
+    "A": {
+    "size": 60,
+    "offsetx": 0,
+    "offsety": -40,
+
+     "triSize":2,
+     "triOffsetx":0,
+     "triOffsety":60,
+     "triRotation":0 
+  },
+    "B": {
+    "size": 60,
+    "offsetx": 0,
+    "offsety": -40,
+
+     "triSize":2,
+     "triOffsetx":0,
+     "triOffsety":60,
+     "triRotation":0 ,
+      },
 //   "C": {
 //     "size": 50,
 //     "offsetx": 15,
